837ee8902c6f52695569485479e96d66d74f2a4a
Jakub Filo <jakub@filo.dev>
2022-09-14 22:57:11 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  env: {
+    node: true
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none'
+      }
+    ]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>Hello World!</template>
+
+<style>
+@import '@/assets/main.css';
+</style>
